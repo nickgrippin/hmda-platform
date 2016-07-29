417c072d2fb7e97bd1488bccb9c108e2e5d72543
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-07-29 11:32:58 -0400

    Add example time directive

---
+++ api/src/main/scala/hmda/api/http/HmdaCustomDirectives.scala
@@ -0,0 +1,19 @@
+package hmda.api.http
+
+import akka.http.scaladsl.server.Directive0
+import akka.http.scaladsl.server.Directives._
+
+trait HmdaCustomDirectives {
+
+  def time: Directive0 = {
+    val startTime = System.currentTimeMillis()
+
+    mapResponse { response =>
+      val endTime = System.currentTimeMillis()
+      println(endTime - startTime)
+      response
+    }
+
+  }
+
+}
