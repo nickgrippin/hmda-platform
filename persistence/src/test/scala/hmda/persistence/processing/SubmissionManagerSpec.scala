3982f4e042703dcac2e1feea11a3501bf584199a
Betsy Lorton <elorton@gmail.com>
2017-02-08 08:56:33 -0500

    Update Filing Status when submission progresses
    - if no submission, filing status = not-started
    - when a filing gets its first submission uploaded, update status to
      in-progress.
    - when a submission is signed, filing status updates to "completed"
    
    This work done by nickgrippin, re-committed by schbetsy

--- persistence/src/test/scala/hmda/persistence/processing/SubmissionManagerSpec.scala
+++ persistence/src/test/scala/hmda/persistence/processing/SubmissionManagerSpec.scala
@@ -2,35 +2,55 @@ package hmda.persistence.processing
 
 import java.time.Instant
 
+import akka.actor.ActorRef
 import akka.testkit.TestProbe
 import com.typesafe.config.ConfigFactory
-import hmda.model.fi.{ SubmissionId, Uploaded, ValidatedWithErrors }
+import hmda.model.fi._
+import hmda.model.util.FITestData._
+import hmda.persistence.HmdaSupervisor.FindFilings
+import hmda.persistence.institutions.FilingPersistence
 import hmda.persistence.messages.CommonMessages.GetState
 import hmda.persistence.model.ActorSpec
 import hmda.persistence.processing.HmdaRawFile.AddLine
 import hmda.persistence.processing.ProcessingMessages.{ CompleteUpload, StartUpload }
 import hmda.persistence.processing.SubmissionManager._
+import hmda.persistence.institutions.FilingPersistence._
 
 class SubmissionManagerSpec extends ActorSpec {
-  import hmda.model.util.FITestData._
-
   val config = ConfigFactory.load()
 
-  val submissionId = SubmissionId("0", "2017", 1)
-
-  val submissionManager = createSubmissionManager(system, submissionId)
+  val submissionId = SubmissionId("0", "testPeriod", 1)
+  val filingPersistence = createFilings(submissionId.institutionId, system)
 
   val probe = TestProbe()
+
+  val filing = Filing(submissionId.period, submissionId.institutionId, NotStarted, filingRequired = false, 0L, 0L)
+  probe.send(filingPersistence, CreateFiling(filing))
+  probe.expectMsg(Some(filing))
+  Thread.sleep(1000)
+
+  val submissionManager = createSubmissionManager(system, submissionId)
 
   val lines = fiCSV.split("\n")
   val timestamp = Instant.now.toEpochMilli
 
   "A HMDA File" must {
     "upload, parse and validate" in {
+      probe.send(filingPersistence, GetFilingByPeriod(submissionId.period))
+      val filingNotStarted = probe.expectMsgType[Filing]
+      filingNotStarted.status mustBe NotStarted
+
       probe.send(submissionManager, StartUpload)
+
       for (line <- lines) {
         probe.send(submissionManager, AddLine(timestamp, line.toString))
       }
+
+      /*Thread.sleep(1000)
+      probe.send(filingPersistence, GetFilingByPeriod(submissionId.period))
+      val filingInProgress = probe.expectMsgType[Filing]
+      filingInProgress.status mustBe InProgress*/
+
       probe.send(submissionManager, CompleteUpload)
       probe.send(submissionManager, GetState)
       probe.expectMsg(Uploaded)
